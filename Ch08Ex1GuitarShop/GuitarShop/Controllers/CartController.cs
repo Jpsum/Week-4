ac538a76130afb0b29bdb853bb33cd36281b74ad
Jpsum <jsumme14@live.spcollege.edu>
2026-09-14 01:14:48 -0400

    Add Ch08Ex1GuitarShop project

---
+++ Ch08Ex1GuitarShop/GuitarShop/Controllers/CartController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GuitarShop.Controllers
+{
+    public class CartController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Add(string id)
+        {
+            TempData["message"] = $"{id} has been added to the cart.";
+            return RedirectToAction("list", "Product");
+        }
+    }
+}
